9710e2fc847dc5758c2d4b6fafe1477dee9f044d
Gabe Rojas-Westall <rojaswestall@gmail.com>
2017-10-20 13:04:14 -0500

    changing back to the updated

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gabriel Rojas-Westall' });
+  res.render('layout', { title: 'Gabriel Rojas-Westall' });
 });
 
 module.exports = router;
